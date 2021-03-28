515924a2f886b152ecc0b8a064e1c80e55a65cdc
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-03-28 23:07:49 +0600

    4 Display a loading spinner on a React Component updated

--- src/components/ProductDetails/ProductDetails.js
+++ src/components/ProductDetails/ProductDetails.js
@@ -1,34 +1,37 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
-// import fakeData from '../../fakeData';
+import fakeData from '../../fakeData';
 import Product from '../Product/Product';
 
 const ProductDetails = () => {
-    const { productKey } = useParams();
+    const {productKey} = useParams();
     // useParams pass kora parameter ke receive korar jonno use kora hoy
 
-    const [Loading, setLoading] = useState('true');
-    const [product, setProduct] = useState({});
+    // const [loading, setLoading] = useState(true);
+    // const [product, setProduct] = useState({});
 
-    useEffect(() => {
-        fetch('/product/'+ productKey)
-            .then(res => res.json())
-            .then(data => {
-                setProduct(data);
-                setLoading(false);
-            })
-    }, [productKey])
+    // useEffect(() => {
+    //     fetch('/product'+productKey)
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         setProduct(data);
+    //         setLoading(false);
+    //     })
+    // }, [productKey])
 
-    // const product = fakeData.find(pd => pd.key === productKey)
+    const product = fakeData.find(pd => pd.key === productKey)
 
     return (
         <div>
             {/* <h1 >{productKey} Details Comming Soon....</h1> */}
             <h1 >Your Product Details</h1>
-            {
-                Loading ? <p>Loading...</p> :
-                <Product showAddToCart={false} product={product}></Product>
-            }
+
+            {/* {
+                loading ? <p>Loading...</p>:
+                    <Product showAddToCart={false} product={product}></Product>
+            } */}
+
+            <Product showAddToCart={false} product={product}></Product>
         </div>
     );
 };
